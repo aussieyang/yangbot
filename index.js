c9b1226f7df5e5d88b0b4b12ce9d7f544a122dd3
aussieyang <yang.j.li91@gmail.com>
2016-07-04 14:44:23 +1000

    heroku port fix

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+//For avoiding Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // access API using node-rest-client
 var Client = require('node-rest-client').Client;
 var client = new Client();
